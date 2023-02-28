b6fa971fa5b2684fface8a408426f139b7f62f35
Gleisson Santos Silva <gtsasil@gmail.com>
2023-02-28 17:22:25 +0000

    End of exercice

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,38 +1,45 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Trash } from "phosphor-react"
 
 
 //create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState("");
-	const [todos, setTodos] = useState([]);
-	return (
-		<div className="container">
-			<h1>My Todo List</h1>
-			<ul>
-				<li>
-					<input 
-					type="text"
-					onChange={(e)=>{setInputValue(e.target.value)}}
-					value={inputValue}
-					onKeyPress={(e) => {
-						if (e.key === "Enter") {
-							setTodos(todos.concat(inputValue));
-							setInputValue("");
-						}		
-					}}
-					placeholder="Add your item here" />
-				</li>
-				{todos.map(item =><li>
-					{item}
-				<button className="trash">
-					<Trash size={24}/>
-				</button>
-				</li>)}
-			</ul>
-			<div>x Task</div>
-		</div>
-	);
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
+    return (
+        <div className="container">
+            <h1>My Todo List</h1>
+            <ul>
+                <li>
+                    <input
+                        type="text"
+                        onChange={(e) => { setInputValue(e.target.value) }}
+                        value={inputValue}
+                        onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                                setTodos(todos.concat(inputValue));
+                                setInputValue("");
+                            }
+                        }}
+                        placeholder="Add your item here" />
+                </li>
+                {todos.map((item, index) =>
+                    <li>
+                        {item}
+                        <button
+                            className="trash"
+                            onClick={() => setTodos(
+                                todos.filter(
+                                    (t, currentIndex) => index != currentIndex
+                                ))}>
+                            <Trash size={24} />
+                        </button>
+                    </li>
+                )}
+            </ul>
+            <div>x Task</div>
+        </div>
+    );
 };
 
 export default Home;
